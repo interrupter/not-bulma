c640d44a5f7ed287a6484bb1333edb379b60934e
Aleksander Repin <alex.repin@mail.ru>
2020-09-10 09:46:49 +0300

    fix: Table cell control 'switch'

---
+++ src/table/controls/ui.switch.svelte
@@ -0,0 +1,45 @@
+<script>
+	import 'bulma-switch';
+  import UICommon from '../../common.js';
+
+  import { createEventDispatcher } from 'svelte';
+	let dispatch = createEventDispatcher();
+
+  export let id = '';
+	export let value = false;
+  export let fieldname = 'switch';
+  export let disabled = false;
+	export let readonly = false;
+  export let styling = " is-rounded is-success ";
+
+  function onBlur(ev){
+  	let data = {
+			id,
+  		field: fieldname,
+  		value: ev.target.type === 'checkbox' ? ev.target.checked:ev.target.value
+  	};
+    dispatch('change', data);
+    return true;
+  }
+
+  function onInput(ev){
+  	let data = {
+			id,
+  		field: fieldname,
+      value
+  	};
+    dispatch('change', data);
+    return true;
+  }
+
+</script>
+
+<input type="checkbox"
+  class="switch {styling}"
+  id="edit-table-row-cell-inline-switch-{fieldname}-{id}"
+  bind:checked={value}
+  name="{fieldname}"
+  {readonly} {disabled}
+  on:blur={onBlur} on:input={onInput}
+  />
+<label class="label" for="edit-table-row-cell-inline-switch-{fieldname}-{id}"></label>
