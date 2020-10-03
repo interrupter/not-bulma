3e10ca5890dc217acb9d86c8ed829c3c36564233
Aleksander Repin <alex.repin@mail.ru>
2020-10-03 14:46:09 +0300

    fixed: top menu section will be display if not empty or if contains indicator or tag

--- src/ui.top.menu.svelte
+++ src/ui.top.menu.svelte
@@ -21,7 +21,7 @@
 
 <div class="navbar-end mr-6">
   {#each sections as section(section.id) }
-  {#if items.filter(t => t.section === section.id).length }
+  {#if items.filter(t => t.section === section.id).length || section.indicator || section.tag }
   <div class="navbar-item has-dropdown is-hoverable is-pulled-right">
     <a href class="navbar-link">{section.title}</a>
     <div class="navbar-dropdown">
