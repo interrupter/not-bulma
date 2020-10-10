54f2673e43522d5faaa05da05170bd56f4f8b4d1
Aleksander Repin <alex.repin@mail.ru>
2020-10-10 16:28:54 +0300

    remove: disabled option from UILink

--- src/ui.link.svelte
+++ src/ui.link.svelte
@@ -7,7 +7,6 @@
   export let outlined = false;
   export let inverted = false;
   export let rounded = false;
-  export let disabled = false;
   export let state = '';
   export let type = '';
   export let color = '';
@@ -26,7 +25,7 @@
   };
 </script>
 
-<a on:click="{action}" href="{url}" {disabled} class="button {classes} {state?`is-${state}`:''} {inverted?`is-inverted`:''} {outlined?`is-outlined`:''} {raised?`is-raised`:''} {rounded?`is-rounded`:''} {light?`is-light`:''} {loading?`is-loading`:''} {color?`is-${color}`:''} {type?`is-${type}`:''} {size?`is-${size}`:''}">
+<a on:click="{action}" href="{url}" class="button {classes} {state?`is-${state}`:''} {inverted?`is-inverted`:''} {outlined?`is-outlined`:''} {raised?`is-raised`:''} {rounded?`is-rounded`:''} {light?`is-light`:''} {loading?`is-loading`:''} {color?`is-${color}`:''} {type?`is-${type}`:''} {size?`is-${size}`:''}">
   {#if icon }
   {#if iconSide === 'left' }
   <span class="icon"><i class="fas fa-{icon} {size?`is-${size}`:''}"></i></span>
