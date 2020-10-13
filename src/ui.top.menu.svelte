5017834c5de5a6b05a20a3239c67c802903e1b40
Aleksander Repin <alex.repin@mail.ru>
2020-10-13 15:50:08 +0300

    added: ui.top.menu.svelte now support actions for onClick of sections and items

--- src/ui.top.menu.svelte
+++ src/ui.top.menu.svelte
@@ -7,7 +7,10 @@
   export let root = '';
   export let navigate = null;
 
-  function onClick(ev) {
+  function onClick(ev, element) {
+    if(Object.prototype.hasOwnProperty.call(element, 'action')){
+      return element.action(ev, element);
+    }
     ev.preventDefault();
     if (typeof navigate === 'function') {
       navigate({
@@ -25,15 +28,13 @@
       sectionsItemsCount[section.id] = items.filter(t => t.section === section.id).length;
     }
   });
-
 </script>
-
 
 <div class="navbar-end mr-6">
   {#each sections as section(section.id) }
   {#if sectionsItemsCount[section.id] || section.indicator || section.tag }
   <div class="navbar-item {sectionsItemsCount[section.id]?'has-dropdown':''} is-hoverable is-pulled-right">
-    <a href class="navbar-link {sectionsItemsCount[section.id]?'':'is-arrowless'}">
+    <a href class="navbar-link {sectionsItemsCount[section.id]?'':'is-arrowless'}" on:click="{(e)=>{onClick(e, section)})}" >
       {section.title}
       {#if section.tag }
       <span class="ml-3 tag is-{section.tag.type} is-pulled-right">{section.tag.label}</span>
@@ -49,7 +50,7 @@
       {#if item.break }
       <hr class="navbar-divider" />
       {/if}
-      <a class="navbar-item" href="{root}{item.url}" data-href="{item.url}" on:click="{onClick}">{item.title}
+      <a class="navbar-item" href="{root}{item.url}" data-href="{item.url}"  on:click="{(e)=>{onClick(e, item)})}">{item.title}
         {#if item.tag }
         <span class="ml-3 tag is-{item.tag.type} is-pulled-right">{item.tag.label}</span>
         {/if}
