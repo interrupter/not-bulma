6a465f5f2df372c97a348ce5b06a6d2c548feda5
Aleksander Repin <alex.repin@mail.ru>
2020-11-12 17:47:06 +0300

    added: support for link for images in Table

--- src/ui.images.svelte
+++ src/ui.images.svelte
@@ -3,7 +3,15 @@
 </script>
 
 {#each values as item (item.url) }
+{#if item.urlFull }
+<a href="{item.urlFull}" alt="{item.title}" >
+  <figure class="image is-64x64">
+    <img class="" alt="{item.title}" src="{item.url}"  crossOrigin="{item.cors}" />
+  </figure>
+</a>
+{:else}
 <figure class="image is-64x64">
   <img class="" alt="{item.title}" src="{item.url}"  crossOrigin="{item.cors}" />
 </figure>
+{/if}
 {/each}
