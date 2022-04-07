34d68ba1bc77f1dbb623b0540059b23f4c5719d6
interrupter <alex.repin@mail.ru>
2022-04-07 03:58:49 +0300

    change: notFormUtils exposed in root of package

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {LOCALE, say, notLocale} from './locale';
 
 const Locale = {LOCALE, say, notLocale};
 
-const { notCommon, COMPONENTS, FIELDS, VARIANTS } = Frame;
+const { notCommon, COMPONENTS, FIELDS, VARIANTS, notFormUtils } = Frame;
 
 Object.keys(Elements.Forms).forEach((fieldtype) => {
   Frame.notFormUtils.addComponent(fieldtype, Elements.Forms[fieldtype]);
@@ -20,7 +20,7 @@ export {
   //application framework
   Frame,
   //shorts for common elements
-  notCommon, COMPONENTS, FIELDS, VARIANTS,
+  notCommon, notFormUtils, COMPONENTS, FIELDS, VARIANTS,
   //localization
   Locale,
   LOCALE, say, notLocale
