c205e9855c74fc00cee25f801420729e62a0c950
interrupter <alex.repin@mail.ru>
2022-06-05 00:59:42 +0300

    added ui list errors

---
+++ src/elements/various/ui.errors.list.svelte
@@ -0,0 +1,18 @@
+<script>
+  import {LOCALE} from '../../locale';
+  export let errors = [];
+  export let show = false;
+  export let classes = '';
+  export let id = 'errors-list';
+
+</script>
+
+{#if Array.isArray(errors) && errors.length }
+<p class="help {classes}" {id}>
+  {#if show }
+  {#each errors as error}
+	<span>{$LOCALE[error]}</span>
+	{/each}
+  {:else}&nbsp;{/if}
+</p>
+{/if}
