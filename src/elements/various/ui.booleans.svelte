e3634b6f73f738adcc8ead1ff8fac8e03305be2e
interrupter <alex.repin@mail.ru>
2022-06-04 19:38:36 +0300

    fix: UI elements booleans added locales support

--- src/elements/various/ui.booleans.svelte
+++ src/elements/various/ui.booleans.svelte
@@ -1,13 +1,15 @@
 <script>
-  export let YES = 'Да';
-  export let NO = 'Нет';
+  import {LOCALE} from '../../locale';
+
+  export let LC_TRUE = 'not-node:booleans_true';
+  export let LC_FALSE = 'not-node:booleans_false';
   export let values = [];
 </script>
 
 {#each values as item }
 {#if item.value }
-<span class="tag is-success">{YES}</span>
+<span class="tag is-success">{$LOCALE[LC_TRUE]}</span>
 {:else}
-<span class="tag is-danger">{NO}</span>
+<span class="tag is-danger">{$LOCALE[LC_FALSE]}</span>
 {/if}
 {/each}
