488d5b35abd99a7b594a003c0cc08ea7c112746f
interrupter <alex.repin@mail.ru>
2022-06-10 18:55:27 +0300

    fix: linter warnings

--- src/elements/form/ui.tag.svelte
+++ src/elements/form/ui.tag.svelte
@@ -1,31 +1,67 @@
 <script>
   import {LOCALE} from '../../locale';
-  import { createEventDispatcher } from 'svelte';
+  import ErrorsList from '../various/ui.errors.list.svelte';
+  import UICommon from '../common.js';
+  import {createEventDispatcher } from 'svelte';
   let dispatch = createEventDispatcher();
-  /*
-  item = {
-    id,        //unique
-    title,     //some text
-    type       //for coloring items, usual html template names danger, success, etc
-  }
-  */
-
-  export let items = [];
+  const LC_ADD = 'not-node:add_label';
+  const LC_SELECT_FROM_LIST = 'not-node:select_from_list_label';
+  //list of item ids
+  export let value = []; //raw ids of variants
+  export let inputStarted = false;
   export let variants = [];
-  export let error = false;
+  export let fieldname = 'tag';
+  export let required = true;
   export let readonly = false;
-  export let beforeAdd = (/*item, list*/)=>{
+  export let valid = true;
+  export let validated = false;
+  export let errors = false;
+  export let formErrors = false;
+  export let formLevelError = false;
+
+  export let beforeAdd = (/*variant, variants*/)=>{
     return true;
   };
+
+  export let getItemId = (variant)=>{
+    return variant.id;
+  };
+
+  export let getItemTitle = (variant)=>{
+    return variant.title;
+  };
+
+  export let getItemType = (variant)=>{
+    return 'info';
+  };
+
+  export let buildItem = (variant)=>{
+    return {
+      id: getItemId(variant),
+      title: getItemTitle(variant),
+      type: getItemType(variant)
+    };
+  };
+
+  function variantIdToVariant(id){
+    return variants.find(variant => getItemId(variant) === id);
+  }
+
+  function changeEvent(){
+    let data = {
+      field: fieldname,
+      value
+    };
+    dispatch('change', data);
+  }
 
   function remove(e){
     e && e.preventDefault();
     let id = e.currentTarget.dataset.id;
-    let item = items.find(el => el.id === id);
-    if(item){
-      items.splice(items.indexOf(item), 1);
-      items = items;
-      dispatch('change', items);
+    if(value.includes(id)){
+      value.splice(value.indexOf(id), 1);
+      value = value;
+      changeEvent();
     }
     return false;
   }
@@ -33,24 +69,37 @@
   function add(e){
     e && e.preventDefault();
     let id = e.currentTarget.parentNode.querySelector('select').value;
-    let item = variants.find(el => el.id === id);
-    if(!beforeAdd(item, items)){
+    const variant = variantIdToVariant(id);
+    if(!variant){
       return false;
     }
-    if(item && (items.indexOf(item) === -1)){
-      items.push(item);
-      items = items;
-      dispatch('change', items);
+    if(!beforeAdd(variant, variants)){
+      return false;
+    }
+    if(id && (value.indexOf(id) === -1)){
+      value.push(id);
+      value = value;
+      changeEvent();
     }
     return false;
   }
-
-  $: classes = error?'is-danger':'';
+  /*
+  item = {
+    id,        //unique
+    title,     //some text
+    type       //for coloring items, usual html template names danger, success, etc
+  }
+  */
+  $: items = value.map(variantIdToVariant).map(buildItem);
+  $: allErrors = [].concat(errors?errors:[], formErrors?formErrors:[]);
+  $: showErrors = (!(validated && valid) && (inputStarted));
+  $: invalid = ((valid===false) || (formLevelError));
+  $: validationClasses = (valid===true || !inputStarted)?UICommon.CLASS_OK:UICommon.CLASS_ERR;
 
 </script>
 
-<div class="columns">
-  <div class="column {classes}">
+<div class="control columns">
+  <div class="column {validationClasses}">
     {#each items as item (item.id)}
     <span class="mx-1 tag is-{item.type}">{$LOCALE[item.title]}
       {#if !readonly }
@@ -64,14 +113,20 @@
     <div class="control">
       <div class="select is-small">
         <select>
-          <option value="-1" selected>{$LOCALE['Выберите из списка...']}</option>
+          <option value="-1" selected>{$LOCALE[LC_SELECT_FROM_LIST]}</option>
           {#each variants as variant}
           <option value="{variant.id}">{$LOCALE[variant.title]}</option>
           {/each}
         </select>
       </div>
-      <button class="button is-primary is-small" on:click={add}>{$LOCALE['Добавить']}</button>
+      <button class="button is-primary is-small" on:click={add}>{$LOCALE[LC_ADD]}</button>
     </div>
   </div>
   {/if}
 </div>
+<ErrorsList
+  bind:errors={allErrors}
+  bind:show={showErrors}
+  bind:classes={validationClasses}
+  id="input-field-helper-{fieldname}"
+  />
--- src/elements/various/ui.tag.svelte
+++ src/elements/various/ui.tag.svelte
@@ -1,136 +1,63 @@
 <script>
+  import {onMount} from 'svelte';
+
   import {LOCALE} from '../../locale';
-  import ErrorsList from '../various/ui.errors.list.svelte';
-  import UICommon from '../common.js';
-  import {createEventDispatcher } from 'svelte';
-  let dispatch = createEventDispatcher();
-  const LC_ADD = 'not-node:add_label';
-  const LC_SELECT_FROM_LIST = 'not-node:select_from_list_label';
-  //list of item ids
-  export let value = []; //raw ids of variants
-  export let inputStarted = false;
-  export let variants = [];
-  export let placeholder = 'empty select item';
-  export let fieldname = 'tag';
-  export let icon = false;
-  export let required = true;
-  export let readonly = false;
-  export let multiple = false;
-  export let size = 8;
-  export let valid = true;
-  export let validated = false;
-  export let errors = false;
-  export let formErrors = false;
-  export let formLevelError = false;
+  import notCommon from '../../frame/common';
+  //if we want to address this tag
+  export let id = 'tagId';
+  export let title = 'tag';
+  export let color = 'info';
+  export let size = 'normal';
 
-  export let beforeAdd = (/*variant, variants*/)=>{
-    return true;
-  };
+  export let padding = 'normal';
+  export let bold = false;
 
-  export let getItemId = (variant)=>{
-    return variant.id;
-  };
+  export let right = false;
+  export let left = false;
+  export let top = false;
+  export let bottom = false;
 
-  export let getItemTitle = (variant)=>{
-    return variant.title;
-  };
+  export let classes = '';
+  let sided = false;
+  $: sided = right || left || top || bottom;
 
-  export let getItemType = (variant)=>{
-    return 'info';
-  };
-
-  export let buildItem = (variant)=>{
-    return {
-      id: getItemId(variant),
-      title: getItemTitle(variant),
-      type: getItemType(variant)
-    };
-  };
-
-  function variantIdToVariant(id){
-    return variants.find(variant => getItemId(variant) === id);
-  }
-
-  function changeEvent(){
-    let data = {
-      field: fieldname,
-      value
-    };
-    dispatch('change', data);
-  }
-
-  function remove(e){
-    e && e.preventDefault();
-    let id = e.currentTarget.dataset.id;
-    if(value.includes(id)){
-      value.splice(value.indexOf(id), 1);
-      value = value;
-      changeEvent();
+  export let events = {};        //events to react on
+  //register event handlers
+  export let register = notCommon.registerWidgetEvents.bind(notCommon);
+  //
+  export let onUpdate = (data)=>{
+    if (Object.prototype.hasOwnProperty.call(data, 'title')){
+      title = data.title;
     }
-    return false;
+  };
+
+  function getStandartUpdateEventName(){
+    return `tag-${id}:update`;
   }
 
-  function add(e){
-    e && e.preventDefault();
-    let id = e.currentTarget.parentNode.querySelector('select').value;
-    const variant = variantIdToVariant(id);
-    if(!variant){
-      return false;
+  onMount(()=>{
+    if (!Object.prototype.hasOwnProperty.call(events, getStandartUpdateEventName())){
+      events[getStandartUpdateEventName()] = onUpdate;
     }
-    if(!beforeAdd(variant, variants)){
-      return false;
-    }
-    if(id && (value.indexOf(id) === -1)){
-      value.push(id);
-      value = value;
-      changeEvent();
-    }
-    return false;
-  }
-  /*
-  item = {
-    id,        //unique
-    title,     //some text
-    type       //for coloring items, usual html template names danger, success, etc
-  }
-  */
-  $: items = value.map(variantIdToVariant).map(buildItem);
-  $: allErrors = [].concat(errors?errors:[], formErrors?formErrors:[]);
-  $: showErrors = (!(validated && valid) && (inputStarted));
-  $: invalid = ((valid===false) || (formLevelError));
-  $: validationClasses = (valid===true || !inputStarted)?UICommon.CLASS_OK:UICommon.CLASS_ERR;
+    register(events);
+  });
 
 </script>
 
-<div class="control columns">
-  <div class="column {validationClasses}">
-    {#each items as item (item.id)}
-    <span class="mx-1 tag is-{item.type}">{$LOCALE[item.title]}
-      {#if !readonly }
-      <button data-id="{item.id}" class="delete is-small" on:click="{remove}"></button>
-      {/if}
-    </span>
-    {/each}
-  </div>
-  {#if !readonly }
-  <div class="column">
-    <div class="control">
-      <div class="select is-small">
-        <select>
-          <option value="-1" selected>{$LOCALE[LC_SELECT_FROM_LIST]}</option>
-          {#each variants as variant}
-          <option value="{variant.id}">{$LOCALE[variant.title]}</option>
-          {/each}
-        </select>
-      </div>
-      <button class="button is-primary is-small" on:click={add}>{$LOCALE[LC_ADD]}</button>
-    </div>
-  </div>
-  {/if}
-</div>
-<ErrorsList
-  bind:errors={allErrors}
-  bind:show={showErrors}
-  bind:classes={validationClasses}
-  id="input-field-helper-{fieldname}"
-  />
+{#if title }
+<span
+  id="tag-{id}"
+  class="
+  tag
+  {bold?'has-text-weight-bold':''}
+  {padding!=='normal'?`is-padded-${padding}`:''}
+  is-{size}
+  is-{color}
+  {sided?'is-sided':''}
+  {right?'is-sided-right':''}
+  {left?'is-sided-left':''}
+  {top?'is-sided-top':''}
+  {bottom?'is-sided-bottom':''}
+  {classes}"
+  >{$LOCALE[title]}</span>
+{/if}
