01e740e2653acdf2448190d31b1d6f588031d4ca
interrupter <alex.repin@mail.ru>
2022-07-04 22:47:25 +0300

    added: optional label property to Elements.Layouts.UISwitch

--- src/elements/form/ui.switch.svelte
+++ src/elements/form/ui.switch.svelte
@@ -1,83 +1,104 @@
 <script>
-  import {LOCALE} from '../../locale';
-  import 'bulma-switch';
-  import UICommon from '../common.js';
-  import ErrorsList from '../various/ui.errors.list.svelte';
-  import UIBooleans from '../various/ui.booleans.svelte';
-  import {createEventDispatcher} from 'svelte';
-  let dispatch = createEventDispatcher();
+    import { LOCALE } from "../../locale";
+    import "bulma-switch";
+    import UICommon from "../common.js";
+    import ErrorsList from "../various/ui.errors.list.svelte";
+    import UIBooleans from "../various/ui.booleans.svelte";
+    import { createEventDispatcher } from "svelte";
+    let dispatch = createEventDispatcher();
 
-export let inputStarted = false;
-export let value = false;
-//export let label = 'textfield';
-export let placeholder = 'input some text here, please';
-export let fieldname = 'textfield';
-export let icon = false;
-export let required = true;
-export let readonly = false;
-export let disabled = false;
-export let valid = true;
-export let styling = " is-rounded is-success ";
-export let validated = false;
-export let errors = false;
-export let formErrors = false;
-export let formLevelError = false;
+    export let inputStarted = false;
+    export let value = false;
+    export let label = "";
+    export let placeholder = "input some text here, please";
+    export let fieldname = "textfield";
+    export let icon = false;
+    export let required = true;
+    export let readonly = false;
+    export let disabled = false;
+    export let valid = true;
+    export let styling = " is-rounded is-success ";
+    export let validated = false;
+    export let errors = false;
+    export let formErrors = false;
+    export let formLevelError = false;
 
-$: iconClasses = (icon? ' has-icons-left ':'') + ' has-icons-right ';
-$: allErrors = [].concat(errors?errors:[], formErrors?formErrors:[]);
-$: showErrors = (!(validated && valid) && (inputStarted));
-$: invalid = ((valid===false) || (formLevelError));
-$: validationClasses = (valid===true || !inputStarted)?UICommon.CLASS_OK:UICommon.CLASS_ERR;
+    $: iconClasses = (icon ? " has-icons-left " : "") + " has-icons-right ";
+    $: allErrors = [].concat(
+        errors ? errors : [],
+        formErrors ? formErrors : []
+    );
+    $: showErrors = !(validated && valid) && inputStarted;
+    $: invalid = valid === false || formLevelError;
+    $: validationClasses =
+        valid === true || !inputStarted
+            ? UICommon.CLASS_OK
+            : UICommon.CLASS_ERR;
 
-function onBlur(ev){
-    let data = {
-      field: fieldname,
-      value: ev.currentTarget.type==='checkbox'?ev.currentTarget.checked: value
-    };
-    inputStarted = true;
-    dispatch('change', data);
-    return true;
-}
+    function onBlur(ev) {
+        let data = {
+            field: fieldname,
+            value:
+                ev.currentTarget.type === "checkbox"
+                    ? ev.currentTarget.checked
+                    : value,
+        };
+        inputStarted = true;
+        dispatch("change", data);
+        return true;
+    }
 
-function onInput(ev){
-    let data = {
-      field: fieldname,
-      value: ev.currentTarget.type === 'checkbox' ? ev.currentTarget.checked:value
-    };
-    inputStarted = true;
-    dispatch('change', data);
-    return true;
-}
-
+    function onInput(ev) {
+        let data = {
+            field: fieldname,
+            value:
+                ev.currentTarget.type === "checkbox"
+                    ? ev.currentTarget.checked
+                    : value,
+        };
+        inputStarted = true;
+        dispatch("change", data);
+        return true;
+    }
 </script>
 
-  <div class="control">
-    {#if readonly }
-      {#if value }
-      <UIBooleans values={[{value: false}]} />
-      {:else}
-      <UIBooleans values={[{value: true}]} />
-      {/if}
+<div class="control">
+    {#if readonly}
+        {#if value}
+            <UIBooleans values={[{ value: false }]} />
+        {:else}
+            <UIBooleans values={[{ value: true }]} />
+        {/if}
     {:else}
-      <UIBooleans values={[{value: false}]} />
-      <input type="checkbox"
-        class="switch {styling}"
-        id="form-field-switch-{fieldname}"
-        bind:checked={value}
-        placeholder="{placeholder}"
-        name="{fieldname}"
-        {disabled} {required} {readonly}
-        invalid="{invalid}"
-        on:blur={onBlur} on:input={onInput}
-        aria-controls="input-field-helper-{fieldname}"
-        aria-describedby="input-field-helper-{fieldname}"
-         />
-        <label class="label" for="form-field-switch-{fieldname}"><UIBooleans values={[{value: true }]} /></label>
+        <UIBooleans values={[{ value: false }]} />
+        <input
+            type="checkbox"
+            class="switch {styling}"
+            id="form-field-switch-{fieldname}"
+            bind:checked={value}
+            {placeholder}
+            name={fieldname}
+            {disabled}
+            {required}
+            {readonly}
+            {invalid}
+            on:blur={onBlur}
+            on:input={onInput}
+            aria-controls="input-field-helper-{fieldname}"
+            aria-describedby="input-field-helper-{fieldname}"
+        />
+        <label class="label" for="form-field-switch-{fieldname}">
+            {#if label}
+                {$LOCALE[label]}
+            {:else}
+                <UIBooleans values={[{ value: true }]} />
+            {/if}
+        </label>
     {/if}
-  </div>
-  <ErrorsList
+</div>
+<ErrorsList
     bind:errors={allErrors}
     bind:show={showErrors}
     bind:classes={validationClasses}
     id="input-field-helper-{fieldname}"
-    />
+/>
