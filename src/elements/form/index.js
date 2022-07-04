5a30413b97196cb8366b9d88392bea87aeab50d2
interrupter <alex.repin@mail.ru>
2022-07-04 19:05:47 +0300

    added: Elements.FOrms.UIRange

--- src/elements/form/index.js
+++ src/elements/form/index.js
@@ -1,33 +1,35 @@
-import UIAutocomplete from './ui.autocomplete.svelte';
-import UICheckboxList from './ui.checkbox.list.svelte';
-import UICheckbox from './ui.checkbox.svelte';
-import UIColor from './ui.color.svelte';
-import UIDate from './ui.date.svelte';
-import UIEmail from './ui.email.svelte';
-import UIHidden from './ui.hidden.svelte';
-import UILabel from './ui.label.svelte';
-import UIPassword from './ui.password.svelte';
-import UISelect from './ui.select.svelte';
-import UISwitch from './ui.switch.svelte';
-import UITag from './ui.tag.svelte';
-import UITelephone from './ui.telephone.svelte';
-import UITextarea from './ui.textarea.svelte';
-import UITextfield from './ui.textfield.svelte';
+import UIAutocomplete from "./ui.autocomplete.svelte";
+import UICheckboxList from "./ui.checkbox.list.svelte";
+import UICheckbox from "./ui.checkbox.svelte";
+import UIColor from "./ui.color.svelte";
+import UIDate from "./ui.date.svelte";
+import UIEmail from "./ui.email.svelte";
+import UIHidden from "./ui.hidden.svelte";
+import UILabel from "./ui.label.svelte";
+import UIPassword from "./ui.password.svelte";
+import UISelect from "./ui.select.svelte";
+import UISwitch from "./ui.switch.svelte";
+import UITag from "./ui.tag.svelte";
+import UITelephone from "./ui.telephone.svelte";
+import UITextarea from "./ui.textarea.svelte";
+import UITextfield from "./ui.textfield.svelte";
+import UIRange from "./ui.range.svelte";
 
 export {
-  UIAutocomplete,
-  UICheckboxList,
-  UICheckbox,
-  UIColor,
-  UIDate,
-  UIEmail,
-  UIHidden,
-  UILabel,
-  UIPassword,
-  UISelect,
-  UISwitch,
-  UITag,
-  UITelephone,
-  UITextarea,
-  UITextfield
+    UIAutocomplete,
+    UICheckboxList,
+    UICheckbox,
+    UIColor,
+    UIDate,
+    UIEmail,
+    UIHidden,
+    UILabel,
+    UIPassword,
+    UISelect,
+    UISwitch,
+    UITag,
+    UITelephone,
+    UITextarea,
+    UITextfield,
+    UIRange,
 };
