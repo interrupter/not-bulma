8aa5a3d9eb7f0e422b0b025366337d342c7e7624
interrupter <alex.repin@mail.ru>
2022-11-11 15:42:19 +0300

    added: notStores returned to Frame

--- src/frame/index.js
+++ src/frame/index.js
@@ -3,67 +3,71 @@
 /*
   Common functions
 */
-import notCommon from './common.js';
+import notCommon from "./common.js";
 
 /*
   framework wide parser for data access
 */
-import notPath from 'not-path';
+import notPath from "not-path";
 
+import notRouter from "./router.js";
 
-import notRouter from './router.js';
-
-import * as notAPI from './api';
+import * as notAPI from "./api";
+import * as notStores from "./stores";
 /*
   basic event handlers and core data modifiers
 */
-import notBase from './base.js';
+import notBase from "./base.js";
 
-import {COMPONENTS, FIELDS, VARIANTS} from './LIB.js';
+import { COMPONENTS, FIELDS, VARIANTS } from "./LIB.js";
 /*
   application main infrastructure setter
 */
-import notApp from './app.js';
+import notApp from "./app.js";
 /*
   user controllers
 */
-import notController from './controller.js';
-import notRecord from './record.js'; //  wrapper for data with server live interactions
-import notInterface from './interface.js'; //  wrapper for data with server live interactions
+import notController from "./controller.js";
+import notRecord from "./record.js"; //  wrapper for data with server live interactions
+import notInterface from "./interface.js"; //  wrapper for data with server live interactions
 
 import {
-  notTable,
-  UIForm,
-  notForm,
-  notFormSet,
-  notFormUtils,
-  notBreadcrumbs,
-  notTopMenu,
-  notSideMenu
-} from './components';
+    notTable,
+    UIForm,
+    notForm,
+    notFormSet,
+    notFormUtils,
+    notBreadcrumbs,
+    notTopMenu,
+    notSideMenu,
+} from "./components";
 
-import notCRUD from './crud/controller.crud.js';
+import notCRUD from "./crud/controller.crud.js";
 
 const ncCRUD = notCRUD; //legacy alias
 
 export {
-  notCommon,
-  notPath,
-  notController,
-  notBase,
-  notRouter,
-  notRecord,
-  notInterface,
-  notApp,
-  notAPI,
-  notCRUD, ncCRUD,
-  COMPONENTS, FIELDS, VARIANTS,
-  notTable,
-  UIForm,
-  notForm,
-  notFormSet,
-  notFormUtils,
-  notBreadcrumbs,
-  notTopMenu,
-  notSideMenu
+    notCommon,
+    notPath,
+    notController,
+    notBase,
+    notRouter,
+    notRecord,
+    notInterface,
+    notApp,
+    notAPI,
+    notStores,
+    notCRUD,
+    ncCRUD,
+    COMPONENTS,
+    FIELDS,
+    VARIANTS,
+    notTable,
+    UIForm,
+    notForm,
+    notFormSet,
+    notFormUtils,
+    notBreadcrumbs,
+    notTopMenu,
+    notSideMenu,
 };
---
+++ src/frame/stores.js
@@ -0,0 +1,56 @@
+import { writable } from "svelte/store";
+
+const ALL = {};
+
+function exist(key) {
+    return Object.hasOwn(ALL, key);
+}
+
+function get(key) {
+    if (exist(key)) {
+        return ALL[key];
+    } else {
+        return false;
+    }
+}
+
+function create(
+    key,
+    props = {
+        raw: [],
+        filtered: [],
+        selected: {},
+    }
+) {
+    if (!exist(key)) {
+        if (Object.keys(props).length > 0) {
+            ALL[key] = {};
+            Object.keys(props).forEach((name) => {
+                ALL[key][name] = writable(props[name]);
+            });
+        } else {
+            throw new Error("store's props wasn't specified");
+        }
+    }
+    return ALL[key];
+}
+
+/**
+ * Creates object that is fake Store
+ * Some time this is useful when you need to initialize local var,
+ * before you could get actual Stores from central storage by its ID
+ *	@params {mixed} val 	data of type that is actual storage will contain
+ * @returns {Object}
+ */
+
+function fake(val) {
+    return {
+        subscribe(f) {
+            f(val);
+            return () => {};
+        },
+        set() {},
+    };
+}
+
+export { create, get, fake };
