fb0347351beb20a0abd314b43f391effdd1659b0
interrupter <alex.repin@mail.ru>
2023-02-03 04:48:28 +0300

    fix: Table cell svelte error field is undefined

--- src/frame/components/table/notTableCell.svelte
+++ src/frame/components/table/notTableCell.svelte
@@ -1,73 +1,70 @@
 <script>
-  import notPath from 'not-path';
-  import {
-    onMount
-  } from 'svelte';
+    import notPath from "not-path";
+    import { onMount } from "svelte";
 
-  let title = '';
+    let title = "";
 
-  onMount(() => {
-    if (typeof field.type === 'undefined') {
-      title = notPath.get(field.path, item, helpers);
-    }
-  });
+    onMount(() => {
+        if (typeof field.type === "undefined") {
+            title = notPath.get(field.path, item, helpers);
+        }
+    });
 
-  import notCommon from '../../common';
+    import notCommon from "../../common";
 
-  import {
-    LOCALE
-  } from '../../../locale';
+    import { LOCALE } from "../../../locale";
 
-  import UIButtons from '../../../elements/button/ui.buttons.svelte';
-  import UILinks from '../../../elements/link/ui.links.svelte';
-  import UIImages from '../../../elements/image/ui.images.svelte';
-  import UIBooleans from '../../../elements/various/ui.booleans.svelte';
+    import UIButtons from "../../../elements/button/ui.buttons.svelte";
+    import UILinks from "../../../elements/link/ui.links.svelte";
+    import UIImages from "../../../elements/image/ui.images.svelte";
+    import UIBooleans from "../../../elements/various/ui.booleans.svelte";
 
-  import TableSwitch from './controls/ui.switch.svelte';
-  import TableTags from './controls/ui.tags.svelte';
+    import TableSwitch from "./controls/ui.switch.svelte";
+    import TableTags from "./controls/ui.tags.svelte";
 
-  export let getItemId = item => item._id;
-  export let field = {};
-  export let item = {};
-  export let helpers = {};
+    export let getItemId = (item) => item._id;
+    export let field = {};
+    export let item = {};
+    export let helpers = {};
 </script>
 
-<td class="{field.hideOnMobile?'is-hidden-touch':''}" {title}>
-  {#if field.type === 'link' }
-  <UILinks values={ notPath.get(field.path, item, helpers) } />
-  {:else if field.type === 'button' }
-  <UIButtons values={ notPath.get(field.path, item, helpers) } />
-  {:else if field.type === 'image' }
-  <UIImages values={ notPath.get(field.path, item, helpers) } />
-  {:else if field.type === 'boolean' }
-  <UIBooleans values={ notPath.get(field.path, item, helpers) } />
-  {:else if field.type === 'tag' }
-  <TableTags values={ notPath.get(field.path, item, helpers) } />
-  {:else if field.type === 'switch' }
-  <TableSwitch
-    id={getItemId(item)}
-    fieldname={field.path}
-    on:change="{field.onChange}"
-    value={ notPath.get(field.path, item, helpers) }
-    disabled={field.disabled}
-    readonly={field.readonly}
-    />
-  {:else if field.type === 'component' }
-  <svelte:component
-    id={getItemId(item)}
-    this={field.component}
-    on:change="{field.onChange}"
-    fieldname={field.path}
-    disabled={field.disabled}
-    readonly={field.readonly}
-    value={ notPath.get(field.path, item, helpers) }
-    {...field.options}
-    />
-  {:else}
-  {#if !isNaN(field.maxLength) && field.maxLength }
-  {notCommon.strLengthCap(notPath.get(field.path, item, helpers), field.maxLength)}
-  {:else}
-  {$LOCALE[notPath.get(field.path, item, helpers)]}
-  {/if}
-  {/if}
+<td class={field.hideOnMobile ? "is-hidden-touch" : ""} {title}>
+    {#if field.type === "link"}
+        <UILinks values={notPath.get(field.path, item, helpers)} />
+    {:else if field.type === "button"}
+        <UIButtons values={notPath.get(field.path, item, helpers)} />
+    {:else if field.type === "image"}
+        <UIImages values={notPath.get(field.path, item, helpers)} />
+    {:else if field.type === "boolean"}
+        <UIBooleans values={notPath.get(field.path, item, helpers)} />
+    {:else if field.type === "tag"}
+        <TableTags values={notPath.get(field.path, item, helpers)} />
+    {:else if field.type === "switch"}
+        <TableSwitch
+            id={getItemId(item)}
+            fieldname={field.path}
+            on:change={field.onChange}
+            value={notPath.get(field.path, item, helpers)}
+            disabled={field.disabled}
+            readonly={field.readonly}
+        />
+    {:else if field.type === "component"}
+        <svelte:component
+            this={field.component}
+            id={getItemId(item)}
+            on:change={field.onChange}
+            fieldname={field.path}
+            disabled={field.disabled}
+            readonly={field.readonly}
+            value={notPath.get(field.path, item, helpers)}
+            {...field.options}
+        />
+    {:else if field && typeof field !== "undefined" && !isNaN(field.maxLength) && field.maxLength}
+        {notCommon.strLengthCap(
+            notPath.get(field.path, item, helpers),
+            field.maxLength
+        )}
+    {:else}
+        {$LOCALE[notPath.get(field.path, item, helpers)]}
+    {/if}
 </td>
