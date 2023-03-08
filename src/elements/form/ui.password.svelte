d518610b82026d77f312b0764b89221ebbdc1dc2
interrupter <alex.repin@mail.ru>
2023-03-08 14:40:16 +0300

    added: CRUD Action List interface options pass thru

--- src/elements/form/ui.password.svelte
+++ src/elements/form/ui.password.svelte
@@ -1,75 +1,91 @@
 <script>
+    import UICommon from "../common.js";
+    import ErrorsList from "../various/ui.errors.list.svelte";
 
-  import UICommon from '../common.js';
-  import ErrorsList from '../various/ui.errors.list.svelte';
+    import { createEventDispatcher } from "svelte";
+    let dispatch = createEventDispatcher();
 
+    export let inputStarted = false;
+    export let value = "";
+    export let placeholder = "input some text here, please";
+    export let fieldname = "password";
+    export let icon = false;
+    export let required = true;
+    export let readonly = false;
+    export let valid = true;
+    export let validated = false;
+    export let errors = false;
+    export let formErrors = false;
+    export let formLevelError = false;
 
-  import {createEventDispatcher} from 'svelte';
-  let dispatch = createEventDispatcher();
+    $: iconClasses = (icon ? " has-icons-left " : "") + " has-icons-right ";
+    $: allErrors = [].concat(
+        errors ? errors : [],
+        formErrors ? formErrors : []
+    );
+    $: showErrors = !(validated && valid) && inputStarted;
+    $: invalid = valid === false || formLevelError;
+    $: validationClasses =
+        valid === true || !inputStarted
+            ? UICommon.CLASS_OK
+            : UICommon.CLASS_ERR;
+    $: {
+    }
 
-  export let inputStarted = false;
-  export let value = '';
-  export let placeholder = 'input some text here, please';
-  export let fieldname = 'password';
-  export let icon = false;
-  export let required = true;
-  export let readonly = false;
-  export let valid = true;
-  export let validated = false;
-  export let errors = false;
-  export let formErrors = false;
-  export let formLevelError = false;
+    function onBlur(ev) {
+        let data = {
+            field: fieldname,
+            value: ev.currentTarget.value,
+        };
+        inputStarted = true;
+        dispatch("change", data);
+        return true;
+    }
 
-  $: iconClasses = (icon? ' has-icons-left ':'') + ' has-icons-right ';
-  $: allErrors = [].concat(errors?errors:[], formErrors?formErrors:[]);
-  $: showErrors = (!(validated && valid) && (inputStarted));
-  $: invalid = ((valid===false) || (formLevelError));
-  $: validationClasses = (valid===true || !inputStarted)?UICommon.CLASS_OK:UICommon.CLASS_ERR;
-
-  function onBlur(ev){
-    let data = {
-      field: fieldname,
-      value: ev.currentTarget.value
-    };
-    inputStarted = true;
-    dispatch('change', data);
-    return true;
-  }
-
-  function onInput(ev){
-    let data = {
-      field: fieldname,
-      value: ev.currentTarget.value
-    };
-    inputStarted = true;
-    dispatch('change', data);
-    return true;
-  }
-
+    function onInput(ev) {
+        let data = {
+            field: fieldname,
+            value: ev.currentTarget.value,
+        };
+        inputStarted = true;
+        dispatch("change", data);
+        return true;
+    }
 </script>
 
-  <div class="control {iconClasses}">
-    <input class="input {validationClasses}" {readonly}
-    id="form-field-password-{fieldname}"
-    type="password" name="{fieldname}" invalid="{invalid}" required={required} placeholder="{placeholder}" bind:value={value} autocomplete="{fieldname}" aria-controls="input-field-helper-{fieldname}"
-      on:change={onBlur} on:input={onInput}
-      aria-describedby="input-field-helper-{fieldname}" />
-    {#if icon }
-    <span class="icon is-small is-left"><i class="fas fa-{icon}"></i></span>
+<div class="control {iconClasses}">
+    <input
+        class="input {validationClasses}"
+        {readonly}
+        id="form-field-password-{fieldname}"
+        type="password"
+        name={fieldname}
+        {invalid}
+        {required}
+        {placeholder}
+        bind:value
+        autocomplete={fieldname}
+        aria-controls="input-field-helper-{fieldname}"
+        on:change={onBlur}
+        on:input={onInput}
+        aria-describedby="input-field-helper-{fieldname}"
+    />
+    {#if icon}
+        <span class="icon is-small is-left"><i class="fas fa-{icon}" /></span>
     {/if}
-    {#if validated === true }
-    <span class="icon is-small is-right">
-      {#if valid === true }
-      <i class="fas fa-check"></i>
-      {:else if (valid === false) }
-      <i class="fas fa-exclamation-triangle"></i>
-      {/if}
-    </span>
+    {#if validated === true}
+        <span class="icon is-small is-right">
+            {#if valid === true}
+                <i class="fas fa-check" />
+            {:else if valid === false}
+                <i class="fas fa-exclamation-triangle" />
+            {/if}
+        </span>
     {/if}
-  </div>
-  <ErrorsList
+</div>
+<ErrorsList
     bind:errors={allErrors}
     bind:show={showErrors}
     bind:classes={validationClasses}
     id="input-field-helper-{fieldname}"
-    />
+/>
--- src/frame/crud/actions/list.js
+++ src/frame/crud/actions/list.js
@@ -1,69 +1,74 @@
-import {notTable} from '../../components';
+import { notTable } from "../../components";
 
-const ACTION = 'list';
+const ACTION = "list";
 
-export default class CRUDActionList{
+export default class CRUDActionList {
+    static async run(controller, params) {
+        try {
+            await controller.preloadVariants(ACTION);
 
-  static async run(controller, params) {
-    try{
-      await controller.preloadVariants(ACTION);
+            controller.setBreadcrumbs([
+                {
+                    title: "Список",
+                    url: controller.getModelURL(),
+                },
+            ]);
 
-      controller.setBreadcrumbs([{
-        title: 'Список',
-        url: controller.getModelURL()
-      }]);
+            if (controller.ui[ACTION]) {
+                return;
+            } else {
+                controller.$destroyUI();
+            }
 
-      if (controller.ui[ACTION]) {
-        return;
-      } else {
-        controller.$destroyUI();
-      }
-
-      controller.ui[ACTION] = new notTable(CRUDActionList.prepareOptions(controller));
-      controller.emit(`after:render:${ACTION}`);
-    }catch(e){
-      controller.report(e);
-      controller.showErrorMessage(e);
+            controller.ui[ACTION] = new notTable(
+                CRUDActionList.prepareOptions(controller)
+            );
+            controller.emit(`after:render:${ACTION}`);
+        } catch (e) {
+            controller.report(e);
+            controller.showErrorMessage(e);
+        }
     }
-  }
 
-  static prepareOptions(controller){
-    const DEFAULT_OPTIONS_TABLE = {
-      interface: {
-        combined: true,
-        factory: controller.getInterface()
-      },
-      fields:       undefined,
-      showSelect:   undefined,
-      getItemId:     undefined,
-      idField:       undefined,
-      preload:       {},
-      pager:         { size: 50, page: 0},
-      sorter:       {
-        id: -1
-      },
-      filter:       undefined,
-    };
-    const TABLE_OPTIONS = {
-      options: {
-        targetEl: controller.getContainerInnerElement(),
-        endless: false,
-        actions: [
-          {
-            title: 'Создать',
-            action: controller.goCreate.bind(controller)
-          },
-          ...(controller.getOptions(`${ACTION}.actions`, []))
-        ],
-      }
-    };
-    Object.keys(DEFAULT_OPTIONS_TABLE).forEach( (key) => {
-      let optVal = controller.getOptions(`${ACTION}.${key}`, DEFAULT_OPTIONS_TABLE[key]);
-      if(typeof optVal !== 'undefined'){
-        TABLE_OPTIONS.options[key] = optVal;
-      }
-    });
-    return TABLE_OPTIONS;
-  }
-
+    static prepareOptions(controller) {
+        const DEFAULT_OPTIONS_TABLE = {
+            interface: controller.getOptions(`${ACTION}.interface`, {
+                combined: true,
+                factory: controller.getInterface(),
+            }),
+            fields: undefined,
+            showSelect: undefined,
+            getItemId: undefined,
+            idField: undefined,
+            preload: {},
+            pager: { size: 50, page: 0 },
+            sorter: {
+                id: -1,
+            },
+            filter: undefined,
+        };
+        const TABLE_OPTIONS = {
+            options: {
+                targetEl: controller.getContainerInnerElement(),
+                endless: false,
+                actions: [
+                    {
+                        title: "Создать",
+                        action: controller.goCreate.bind(controller),
+                    },
+                    ...controller.getOptions(`${ACTION}.actions`, []),
+                ],
+            },
+        };
+        Object.keys(DEFAULT_OPTIONS_TABLE).forEach((key) => {
+            let optVal = controller.getOptions(
+                `${ACTION}.${key}`,
+                DEFAULT_OPTIONS_TABLE[key]
+            );
+            if (typeof optVal !== "undefined") {
+                TABLE_OPTIONS.options[key] = optVal;
+            }
+        });
+        return TABLE_OPTIONS;
+    }
 }
