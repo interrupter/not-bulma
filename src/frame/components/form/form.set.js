330b39163b6e173a16781922d952ed589554a8ce
interrupter <alex.repin@mail.ru>
2023-03-09 21:41:46 +0300

    add: changeMode:func option now passed by notFormSet to notForm when constructing

--- src/frame/components/form/form.set.js
+++ src/frame/components/form/form.set.js
@@ -1,21 +1,21 @@
-import Lib from '../../lib.js';
-import notCommon from '../../common';
-import notBase from '../../base';
+import Lib from "../../lib.js";
+import notCommon from "../../common";
+import notBase from "../../base";
 
-import UIFormSetComponent from './form.set.svelte';
-import UIFormComponent from './form.svelte';
+import UIFormSetComponent from "./form.set.svelte";
+import UIFormComponent from "./form.svelte";
 
-const DEFAULT_CONTAINER_SELECTOR = '.form-set';
-const DEFAULT_FORM_SET_NAME = 'form-set';
+const DEFAULT_CONTAINER_SELECTOR = ".form-set";
+const DEFAULT_FORM_SET_NAME = "form-set";
 
 class notFormSet extends notBase {
-  #formSetComponent = null;
-  #formComponent = null;
+    #formSetComponent = null;
+    #formComponent = null;
 
-  #form = null;
-  #frame = null;
+    #form = null;
+    #frame = null;
 
-  /*
+    /*
   new notFormSet({
     options:{
       target: el,
@@ -36,157 +36,166 @@ class notFormSet extends notBase {
   });
   */
 
-  constructor({
-    options = {},
-    formComponent = UIFormComponent,
-    formSetComponent = UIFormSetComponent,
-  }) {
-    super({
-      options: {
-        name: DEFAULT_FORM_SET_NAME,
-        mode: 'default',
-        showModes: true,
-        ...options
-      }
-    });
-    this.#formComponent = UIFormComponent;
-    this.#formSetComponent = UIFormSetComponent;
-    this.setFormMode(this.getOptions('mode'));
-    this.initUI();
-  }
-
-  /**
-   * Initalizing form frame mode, with switchers between modes
-   **/
-  initUI() {
-    const target = this.getFrameTargetEl();
-    while(target.children.length) target.removeChild(target.firstChild);
-    this.#frame = new this.#formSetComponent({
-      target,
-      props: this.#getFrameProps()
-    });
-    this.#frame.$on('mode', (ev) => {
-      this.setFormMode(ev.detail);
-      this.updateForm();
-    });
-    this.updateForm();
-  }
-
-  setFormMode(name){
-    if (this.isModeExists(name)){
-      this.setWorking('mode', name);
-    }else{
-      this.setWorking('mode', this.getFirstMode());
-      this.updateFormModeInUI();
+    constructor({
+        options = {},
+        formComponent = UIFormComponent,
+        formSetComponent = UIFormSetComponent,
+    }) {
+        super({
+            options: {
+                name: DEFAULT_FORM_SET_NAME,
+                mode: "default",
+                showModes: true,
+                ...options,
+            },
+        });
+        this.#formComponent = UIFormComponent;
+        this.#formSetComponent = UIFormSetComponent;
+        this.setFormMode(this.getOptions("mode"));
+        this.initUI();
     }
-  }
 
-  updateFormModeInUI(){
-    if (this.#frame && this.getWorking('mode') !== null){
-      this.#frame.$set({mode: this.getWorking('mode')})
+    /**
+     * Initalizing form frame mode, with switchers between modes
+     **/
+    initUI() {
+        const target = this.getFrameTargetEl();
+        while (target.children.length) target.removeChild(target.firstChild);
+        this.#frame = new this.#formSetComponent({
+            target,
+            props: this.#getFrameProps(),
+        });
+        this.#frame.$on("mode", (ev) => {
+            this.setFormMode(ev.detail);
+            this.updateForm();
+        });
+        this.updateForm();
     }
-  }
 
-  getFormMode(){
-    return this.getWorking('mode');
-  }
-
-  updateForm(){
-    this.destroyForm();
-    if(this.getWorking('mode') !== null){
-      this.renderForm();
+    setFormMode(name) {
+        if (this.isModeExists(name)) {
+            this.setWorking("mode", name);
+        } else {
+            this.setWorking("mode", this.getFirstMode());
+            this.updateFormModeInUI();
+        }
     }
-  }
 
-  renderForm(){
-    const targetEl = this.getFormTargetEl();
-    const formConfig = this.getFormConfig();
-    if(!(targetEl instanceof HTMLElement && formConfig)){
-      throw new Error('error while form rendering');
+    updateFormModeInUI() {
+        if (this.#frame && this.getWorking("mode") !== null) {
+            this.#frame.$set({ mode: this.getWorking("mode") });
+        }
     }
-    if(formConfig.form){
-      while(targetEl.children.length) targetEl.removeChild(targetEl.firstChild);
-      this.#form = new formConfig.form({options:{target: targetEl}});
-    }else if(formConfig.props){
-      this.#form = new this.#formComponent({
-        target: targetEl,
-        ...formConfig.props
-      });
+
+    getFormMode() {
+        return this.getWorking("mode");
     }
-  }
 
-  getFormConfig(){
-    return this.getOptions('forms').find(form => form.mode === this.getFormMode());
-  }
-
-  destroyForm(){
-    const containerEl = this.getFormTargetEl();
-    if(containerEl){
-      while (containerEl.firstChild) {
-        containerEl.removeChild(containerEl.lastChild);
-      }
+    updateForm() {
+        this.destroyForm();
+        if (this.getWorking("mode") !== null) {
+            this.renderForm();
+        }
     }
-    if (this.#form && this.#form.$destroy){
-      this.#form.$destroy();
+
+    renderForm() {
+        const targetEl = this.getFormTargetEl();
+        const formConfig = this.getFormConfig();
+        if (!(targetEl instanceof HTMLElement && formConfig)) {
+            throw new Error("error while form rendering");
+        }
+        const changeMode = (mode) => {
+            this.setFormMode(mode);
+            this.updateForm();
+        };
+        if (formConfig.form) {
+            while (targetEl.children.length)
+                targetEl.removeChild(targetEl.firstChild);
+            this.#form = new formConfig.form({
+                options: { target: targetEl, changeMode },
+            });
+        } else if (formConfig.props) {
+            this.#form = new this.#formComponent({
+                target: targetEl,
+                ...formConfig.props,
+                changeMode,
+            });
+        }
     }
-    this.#form = null;
-  }
 
-  destroyFrame(){
-    if (this.#frame && this.#frame.$destroy){
-      this.#frame.$destroy();
+    getFormConfig() {
+        return this.getOptions("forms").find(
+            (form) => form.mode === this.getFormMode()
+        );
     }
-    this.#frame = null;
-  }
 
-  isModeExists(mode){
-    const forms = this.getOptions('forms', []);
-    return forms.some(item => item.mode === mode);
-  }
-
-  getFirstMode(){
-    const forms = this.getOptions('forms', []);
-    if(forms.length > 0){
-      return forms[0].mode;
+    destroyForm() {
+        const containerEl = this.getFormTargetEl();
+        if (containerEl) {
+            while (containerEl.firstChild) {
+                containerEl.removeChild(containerEl.lastChild);
+            }
+        }
+        if (this.#form && this.#form.$destroy) {
+            this.#form.$destroy();
+        }
+        this.#form = null;
     }
-    return null;
-  }
 
-  destroy() {
-    this.#formSetComponent = null;
-    this.#formComponent = null;
-    this.destroyForm();
-    this.destroyFrame();
-    thi.setData(null);
-    thi.setOptions(null);
-    thi.setWorking(null);
-  }
-
-  getFrameTargetEl() {
-    const target = this.getOptions('target', DEFAULT_CONTAINER_SELECTOR);
-    if (target instanceof HTMLElement) {
-      return target
-    } else if (typeof target === 'string') {
-      return document.querySelector(target);
-    } else {
-      throw new Error('form set target is not HTMLElement or string');
+    destroyFrame() {
+        if (this.#frame && this.#frame.$destroy) {
+            this.#frame.$destroy();
+        }
+        this.#frame = null;
     }
-  }
 
-  getFormTargetEl(){
-    const name = this.getOptions('name', DEFAULT_FORM_SET_NAME);
-    return document.querySelector(`#${name}-form-set-container`);
-  }
+    isModeExists(mode) {
+        const forms = this.getOptions("forms", []);
+        return forms.some((item) => item.mode === mode);
+    }
 
-  #getFrameProps() {
-    return {
-      showModes: this.getOptions('showModes', true),
-      mode: this.getFormMode(),
-      forms:this.getOptions('forms', []),
-      name: this.getOptions('name', DEFAULT_FORM_SET_NAME),
-    };
-  }
+    getFirstMode() {
+        const forms = this.getOptions("forms", []);
+        if (forms.length > 0) {
+            return forms[0].mode;
+        }
+        return null;
+    }
 
+    destroy() {
+        this.#formSetComponent = null;
+        this.#formComponent = null;
+        this.destroyForm();
+        this.destroyFrame();
+        thi.setData(null);
+        thi.setOptions(null);
+        thi.setWorking(null);
+    }
+
+    getFrameTargetEl() {
+        const target = this.getOptions("target", DEFAULT_CONTAINER_SELECTOR);
+        if (target instanceof HTMLElement) {
+            return target;
+        } else if (typeof target === "string") {
+            return document.querySelector(target);
+        } else {
+            throw new Error("form set target is not HTMLElement or string");
+        }
+    }
+
+    getFormTargetEl() {
+        const name = this.getOptions("name", DEFAULT_FORM_SET_NAME);
+        return document.querySelector(`#${name}-form-set-container`);
+    }
+
+    #getFrameProps() {
+        return {
+            showModes: this.getOptions("showModes", true),
+            mode: this.getFormMode(),
+            forms: this.getOptions("forms", []),
+            name: this.getOptions("name", DEFAULT_FORM_SET_NAME),
+        };
+    }
 }
 
 export default notFormSet;
