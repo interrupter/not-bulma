251c7e3689dfdfb7cd5e8044cfeafa79dc92946b
interrupter <alex.repin@mail.ru>
2023-03-15 23:00:09 +0300

    added: UITitle added id and scrollToTop function

--- src/elements/various/ui.title.svelte
+++ src/elements/various/ui.title.svelte
@@ -1,27 +1,36 @@
 <script>
-  import {LOCALE} from '../../locale';
-  
-  export let title = '';
-  export let subtitle;
-  export let size = 1;
-  export let subsize;
-  export let spaced = false;
-  export let align = 'left';
+    import { LOCALE } from "../../locale";
 
-  let size2;
-  $: size2 = subsize?subsize:(parseInt(size)<6?parseInt(size)+1:size);
-  $: spacedStyle = (spaced?'is-spaced':'');
+    export let id = `title-${Math.random()}`;
+    export let title = "";
+    export let subtitle;
+    export let size = 1;
+    export let subsize;
+    export let spaced = false;
+    export let align = "left";
 
-  $: resultTitle = `<h${size} style="text-align: ${align};" class="title ${spacedStyle} is-${size}">${$LOCALE[title]}</h${size}>`;
-  $: resultSubtitle = `<h${size2} style="text-align: ${align};" class="subtitle is-${size2}">${$LOCALE[subtitle]}</h${size2}>`;
+    export const scrollToTop = () => {
+        setTimeout(() => {
+            document.getElementById(id).scrollIntoView(true);
+        }, 100);
+    };
 
+    let size2;
+    $: size2 = subsize
+        ? subsize
+        : parseInt(size) < 6
+        ? parseInt(size) + 1
+        : size;
+    $: spacedStyle = spaced ? "is-spaced" : "";
+
+    $: resultTitle = `<h${size} id="${id}" style="text-align: ${align};" class="title ${spacedStyle} is-${size}">${$LOCALE[title]}</h${size}>`;
+    $: resultSubtitle = `<h${size2} id="${id}" style="text-align: ${align};" class="subtitle is-${size2}">${$LOCALE[subtitle]}</h${size2}>`;
 </script>
 
-
-{#if title }
-{@html resultTitle }
+{#if title}
+    {@html resultTitle}
 {/if}
 
-{#if subtitle }
-{@html resultSubtitle }
+{#if subtitle}
+    {@html resultSubtitle}
 {/if}
