4634f71ddea8c1bd703e45123cbbc6726f3068d4
interrupter <alex.repin@mail.ru>
2023-04-05 21:21:01 +0300

    added: overlayClasses prop to UIModal and classes to UIOverlay

--- src/elements/modal/ui.modal.svelte
+++ src/elements/modal/ui.modal.svelte
@@ -1,40 +1,47 @@
 <script>
-  import {LOCALE} from '../../locale';
+    import { LOCALE } from "../../locale";
 
-  import UIOverlay from './ui.overlay.svelte';
+    import UIOverlay from "./ui.overlay.svelte";
 
-  import UIBox from '../block/ui.box.svelte';
-  import UIContent from '../block/ui.content.svelte';
+    import UIBox from "../block/ui.box.svelte";
+    import UIContent from "../block/ui.content.svelte";
 
-  import UITitle from '../various/ui.title.svelte';
-  import UIButton from '../button/ui.button.svelte';
+    import UITitle from "../various/ui.title.svelte";
+    import UIButton from "../button/ui.button.svelte";
 
-  export let closeButton = false;
-  export let applyButton = false;
-  export let show = false;
-  export let loading = false;
-  export let title = 'Modal window';
-  export let subtitle = '';
-  export let classes = '';
+    export let closeButton = false;
+    export let applyButton = false;
+    export let show = false;
+    export let loading = false;
+    export let title = "Modal window";
+    export let subtitle = "";
+    export let classes = "";
+    export let overlayClasses = "";
 
-  export let WAITING_TEXT = 'Обработка';
-
+    export let WAITING_TEXT = "Обработка";
 </script>
 
-<UIOverlay {show} closeOnClick={false} closeButton={false} >
-  <UIBox {classes}>
-      <UITitle size="2" title={$LOCALE[title]} subtitle={$LOCALE[subtitle]} />
-			<UIContent>
-        <div class="pageloader {loading?'is-active':''}"><span class="title">{$LOCALE[WAITING_TEXT]}</span></div>
-				<slot></slot>
-				<div class="buttons is-grouped is-centered mt-4">
-          {#if closeButton}
-					<UIButton {...closeButton} />
-          {/if}
-          {#if applyButton}
-					<UIButton {...applyButton} />
-          {/if}
-				</div>
-			</UIContent>
-	</UIBox>
+<UIOverlay
+    {show}
+    closeOnClick={false}
+    closeButton={false}
+    classes={overlayClasses}
+>
+    <UIBox {classes}>
+        <UITitle size="2" title={$LOCALE[title]} subtitle={$LOCALE[subtitle]} />
+        <UIContent>
+            <div class="pageloader {loading ? 'is-active' : ''}">
+                <span class="title">{$LOCALE[WAITING_TEXT]}</span>
+            </div>
+            <slot />
+            <div class="buttons is-grouped is-centered mt-4">
+                {#if closeButton}
+                    <UIButton {...closeButton} />
+                {/if}
+                {#if applyButton}
+                    <UIButton {...applyButton} />
+                {/if}
+            </div>
+        </UIContent>
+    </UIBox>
 </UIOverlay>
--- src/elements/modal/ui.overlay.svelte
+++ src/elements/modal/ui.overlay.svelte
@@ -1,77 +1,78 @@
 <script>
-	let overflowSave = '';
+    let overflowSave = "";
 
-	import {
-	  fade
-	} from 'svelte/transition';
+    import { fade } from "svelte/transition";
 
-	import {
-	  createEventDispatcher,
-	  onMount,
-	  onDestroy
-	} from 'svelte';
+    import { createEventDispatcher, onMount, onDestroy } from "svelte";
 
-	const zIndexStep = 1000;
+    const zIndexStep = 1000;
 
-	const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-	export let closeButton = false;
-	export let show = true;
-	export let closeOnClick = true;
-	export let closeSize = 'normal';
-	export let layer = 1;
+    export let closeButton = false;
+    export let show = true;
+    export let closeOnClick = true;
+    export let closeSize = "normal";
+    export let layer = 1;
+    export let classes = "";
 
-	$: if(show){
-	  document.body.style.overflow = 'hidden';
-	}else{
-	  document.body.style.overflow = overflowSave;
-	}
+    $: if (show) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = overflowSave;
+    }
 
-	function overlayClick(e){
-	  if(closeOnClick){
-	    closeOverlay(e);
-	  }
-	}
+    function overlayClick(e) {
+        if (closeOnClick) {
+            closeOverlay(e);
+        }
+    }
 
-	function closeButtonClick(){
-	  rejectOverlay();
-	}
+    function closeButtonClick() {
+        rejectOverlay();
+    }
 
-	function closeOverlay(e) {
-		try{
-			if(e && e.originalTarget){
-				const target = e.originalTarget;
-				if(target.classList && target.classList.contains('is-overlay')){
-					rejectOverlay();
-				}	   			
-	  		}
-		}
-		catch(_){}
-	}
+    function closeOverlay(e) {
+        try {
+            if (e && e.originalTarget) {
+                const target = e.originalTarget;
+                if (
+                    target.classList &&
+                    target.classList.contains("is-overlay")
+                ) {
+                    rejectOverlay();
+                }
+            }
+        } catch (_) {}
+    }
 
-	function rejectOverlay(data = {}) {
-	  dispatch('reject', data);
-	}
-/*
+    function rejectOverlay(data = {}) {
+        dispatch("reject", data);
+    }
+    /*
 	function resolveOverlay(data = {}) {
 	  dispatch('resolve', data);
 	}
 */
-	onMount(() => {
-	  overflowSave = document.body.style.overflow;
+    onMount(() => {
+        overflowSave = document.body.style.overflow;
+    });
 
-	});
-
-	onDestroy(() => {
-	  document.body.style.overflow = overflowSave;
-	});
+    onDestroy(() => {
+        document.body.style.overflow = overflowSave;
+    });
 </script>
 
 {#if show}
-<div class="is-overlay not-overlay" transition:fade on:click={overlayClick} style="z-index: {zIndexStep * layer};">
-	{#if closeButton}
-  <button on:click={closeButtonClick} class="delete is-{closeSize}"></button>
-	{/if}
-	<slot></slot>
-</div>
+    <div
+        class="is-overlay not-overlay {classes}"
+        transition:fade
+        on:click={overlayClick}
+        style="z-index: {zIndexStep * layer};"
+    >
+        {#if closeButton}
+            <button on:click={closeButtonClick} class="delete is-{closeSize}" />
+        {/if}
+        <slot />
+    </div>
 {/if}
