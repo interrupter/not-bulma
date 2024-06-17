e7ae4b77929d607631aa4528372a090d42d9faaf
Aleksandr Repin <admin@appmon.ru>
2024-06-17 21:54:39 +0300

    change: added UICommon to root exports

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import './scss/style.scss';
+import "./scss/style.scss";
 
-import * as Elements from './elements';
+import * as Elements from "./elements";
 
-import * as Frame from './frame';
+import * as Frame from "./frame";
 
-import {LOCALE, say, notLocale} from './locale';
+import { LOCALE, say, notLocale } from "./locale";
 
-const Locale = {LOCALE, say, notLocale};
+const Locale = { LOCALE, say, notLocale };
+const UICommon = Elements.UICommon;
 
 const { notCommon, COMPONENTS, FIELDS, VARIANTS, notFormUtils } = Frame;
 
-Object.keys(Elements).forEach(componentsSetName=>{
-  Object.keys(Elements[componentsSetName]).forEach((componentName) => {
-    Frame.notFormUtils.addComponent(componentName, Elements[componentsSetName][componentName]);
-  });
+Object.keys(Elements).forEach((componentsSetName) => {
+    Object.keys(Elements[componentsSetName]).forEach((componentName) => {
+        Frame.notFormUtils.addComponent(
+            componentName,
+            Elements[componentsSetName][componentName]
+        );
+    });
 });
 
-
 export {
-  //UI
-  Elements,
-  //application framework
-  Frame,
-  //shorts for common elements
-  notCommon, notFormUtils, COMPONENTS, FIELDS, VARIANTS,
-  //localization
-  Locale,
-  LOCALE, say, notLocale
+    //UI
+    Elements,
+    UICommon,
+    //application framework
+    Frame,
+    //shorts for common elements
+    notCommon,
+    notFormUtils,
+    COMPONENTS,
+    FIELDS,
+    VARIANTS,
+    //localization
+    Locale,
+    LOCALE,
+    say,
+    notLocale,
 };
