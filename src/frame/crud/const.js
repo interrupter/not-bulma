901d1168389d3c02f0135709b235e8c9732a3149
Aleksandr Repin <admin@appmon.ru>
2024-08-20 11:07:18 +0300

    fix: default request result transformer

--- src/frame/crud/const.js
+++ src/frame/crud/const.js
@@ -1,3 +1,7 @@
-const DEFAULT_TRASFORMER = (res) => res;
+const DEFAULT_TRASFORMER = (res) => {
+    return Object.hasOwn(res, "status") && Object.hasOwn(res, "result")
+        ? res.result
+        : res;
+};
 
 export { DEFAULT_TRASFORMER };
