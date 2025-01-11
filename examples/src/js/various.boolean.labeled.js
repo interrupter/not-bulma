339314b06390b496f0e1f3806ab1dfcbc9c764c8
Aleksandr Repin <admin@appmon.ru>
2025-01-11 21:26:40 +0300

    refactoring: Elements.Various.UIBooleanLabeled, docs, examples

---
+++ examples/src/js/various.boolean.labeled.js
@@ -0,0 +1,39 @@
+import { createExamplesSetForMutations } from "./examples.helpers.js";
+
+if (!window.EXAMPLES) {
+    window.EXAMPLES = {};
+}
+
+const INIT_OBJ = {
+    LC_TRUE: "Да",
+    LC_FALSE: "Нет",
+};
+
+const Examples = [
+    {
+        title: "Simple",
+        description: "Simple",
+        props: createExamplesSetForMutations(INIT_OBJ, [
+            {
+                value: true,
+                label: "label +",
+            },
+            {
+                value: false,
+                label: "label - ",
+            },
+            {
+                label: "no value",
+            },
+            {
+                inverted: true,
+                label: "!label",
+            },
+        ]),
+    },
+];
+
+window.EXAMPLES.UIBooleanLabeled = {
+    constructor: "Elements.Various.UIBooleanLabeled",
+    list: Examples,
+};
--- src/elements/various/ui.boolean.labeled.svelte
+++ src/elements/various/ui.boolean.labeled.svelte
@@ -4,9 +4,9 @@
 
     /**
      * @typedef {Object} Props
-     * @property {any} value
-     * @property {any} label
-     * @property {boolean} [inverted]
+     * @property {boolean} value
+     * @property {boolean} [inverted = false]
+     * @property {string} label
      */
 
     /** @type {Props} */
