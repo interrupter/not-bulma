937a79465b5e171d14070dc1b274d8679a4bcdfc
Aleksandr Repin <admin@appmon.ru>
2025-01-13 21:22:15 +0300

    refactoring: Elements.Various.UITag, docs, examples

---
+++ examples/dist/js/various.tag.js
@@ -0,0 +1,29 @@
+if (!window.EXAMPLES) {
+    window.EXAMPLES = {};
+}
+
+const Examples = [{
+    title: "Few variants",
+    props: [{
+        title: "Tag 1",
+        color: "success"
+    }, {
+        title: "Tag 2!",
+        color: "warning",
+        size: "small"
+    }, {
+        title: "Clickable tag!",
+        color: "success",
+        size: "large",
+        bold: true,
+        padding: "large",
+        action: () => {
+            alert("alerting via Tag action!");
+        }
+    }]
+}];
+
+window.EXAMPLES.UITag = {
+    constructor: "Elements.Various.UITag",
+    list: Examples
+};
--- src/elements/various/ui.tag.svelte
+++ src/elements/various/ui.tag.svelte
@@ -4,32 +4,30 @@
     import { LOCALE } from "../../locale";
     import notCommon from "../../frame/common";
 
-    let sided = $state(false);
-
     /**
      * @typedef {Object} Props
-     * @property {string} [id] - if we want to address this tag
-     * @property {string} [title]
-     * @property {string} [color]
-     * @property {string} [size]
-     * @property {string} [padding]
-     * @property {boolean} [bold]
-     * @property {boolean} [right]
-     * @property {boolean} [left]
-     * @property {boolean} [top]
-     * @property {boolean} [bottom]
-     * @property {string} [class]
-     * @property {any} [events]
-     * @property {any} [register] - register event handlers
-     * @property {any} [onUpdate]
-     * @property {any} [action]
-     * @property {object} [vars]
+     * @property {string}   [id = "tagId"]                                  if we want to address this tag, as tag-{id}:{eventName}, ex. tag-tagId:update
+     * @property {string}   [title = "tag"]                                 tag title
+     * @property {string}   [color = "info"]                                bulma color
+     * @property {string}   [size = "normal"]                               bulma size
+     * @property {string}   [padding = "normal"]                            size of left/right paddings (small = 0.5em, normal = 1em, big = 1.5em, large = 2em), class names is is-padded-{padding}
+     * @property {boolean}  [bold = false]                                  title styling
+     * @property {boolean}  [right = false]                                 tag positioning on the right
+     * @property {boolean}  [left = false]                                  tag positioning on the left
+     * @property {boolean}  [top = false]                                   tag positioning on the top
+     * @property {boolean}  [bottom = false]                                tag positioning on the bottom
+     * @property {string}   [class  = ""]                                   additional css classes list
+     * @property {object}   [events = {}]                                   list of events {name:callback}
+     * @property {function} [register = notCommon.registerWidgetEvents]     register event handlers
+     * @property {function} [onUpdate = (data) => title = data.title;]
+     * @property {function} [action]                                        onclick/onkeydown callback
+     * @property {object}   [vars = {}]                                     css vars list
      */
 
     /** @type {Props} */
     let {
         id = "tagId",
-        title = $bindable("tag"),
+        title = "tag",
         color = "info",
         size = "normal",
         padding = "normal",
@@ -39,17 +37,17 @@
         top = false,
         bottom = false,
         class: classes = "",
-        events = $bindable({}),
+        events = {},
         register = notCommon.registerWidgetEvents.bind(notCommon),
         onUpdate = (data) => {
             if (Object.hasOwn(data, "title")) {
                 title = data.title;
             }
         },
-        action = () => {
-            return true;
-        },
+        action,
         vars = {},
+        role = "button",
+        tabIndex = "0",
     } = $props();
 
     function getStandartUpdateEventName() {
@@ -63,8 +61,9 @@
             events[getStandartUpdateEventName()] = onUpdate;
         }
         register(events);
-        sided = right || left || top || bottom;
     });
+
+    let sided = $derived(right || left || top || bottom);
 
     $effect(() => {
         style = Object.keys($state.snapshot(vars))
@@ -77,23 +76,24 @@
 
 {#if title}
     <span
-        role="button"
-        tabindex="0"
+        {role}
+        {tabIndex}
         onkeydown={action ? action : undefined}
         onclick={action ? action : undefined}
         id="tag-{id}"
         class="
   tag
-  {bold ? 'has-text-weight-bold' : ''}
   {padding !== 'normal' ? `is-padded-${padding}` : ''}
   is-{size}
-  is-{color}
-  {sided ? 'is-sided' : ''}
-  {right ? 'is-sided-right' : ''}
-  {left ? 'is-sided-left' : ''}
-  {top ? 'is-sided-top' : ''}
-  {bottom ? 'is-sided-bottom' : ''}
+  is-{color}  
   {classes}"
+        class:is-clickable={action}
+        class:has-text-weight-bold={bold}
+        class:is-sided={sided}
+        class:is-sided-right={right}
+        class:is-sided-left={left}
+        class:is-sided-top={top}
+        class:is-sided-bottom={bottom}
         {style}>{$LOCALE[title]}</span
     >
 {/if}
