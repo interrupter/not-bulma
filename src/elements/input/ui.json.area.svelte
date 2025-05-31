cb5b1c0ee1d36136fbcbc45f2d00ea413b6ddbe6
Aleksandr Repin <admin@appmon.ru>
2025-05-31 22:57:26 +0300

    refactoring of: UIJSONArea and docs, examples

--- src/elements/input/ui.json.area.svelte
+++ src/elements/input/ui.json.area.svelte
@@ -9,6 +9,10 @@
         valid = $bindable(true),
         onchange = () => true,
         onerror = () => {},
+        reactOn = ["onblur"],
+        colorValid = "success",
+        colorInvalid = "danger",
+        validationDelay = 1000,
         ...others
     } = $props();
 
@@ -16,25 +20,48 @@
 
     onMount(() => {
         try {
-            _value = JSON.stringify(value, null, 4);
+            _value = JSON.stringify($state.snapshot(value), null, 4);
         } catch {
             _value = "{}";
         }
     });
 
-    function onChange({ value: val }) {
+    let delayedValidation;
+
+    function onChange(val) {
         try {
             value = JSON.parse(val);
             valid = true;
             onchange({
                 field: fieldname,
-                value,
+                value: $state.snapshot(value),
             });
         } catch (error) {
-            onerror(error.message);
+            valid = false;
+            onerror(error);
         }
         return true;
     }
+
+    function delayedOnChange(data) {
+        const { value: val } = data;
+        if (delayedValidation) {
+            clearTimeout(delayedValidation);
+        }
+        delayedValidation = setTimeout(() => {
+            onChange(val);
+        }, validationDelay);
+    }
+
+    let color = $derived(valid ? colorValid : colorInvalid);
 </script>
 
-<UITextarea bind:value={_value} onchange={onChange} {valid} {...others} />
+<UITextarea
+    bind:value={_value}
+    {fieldname}
+    onchange={delayedOnChange}
+    {reactOn}
+    {valid}
+    {color}
+    {...others}
+/>
